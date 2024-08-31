52c13aa98230055bbf621fbb981d1775d61c6619
Mykola Maik <mykoluamaik@gmail.com>
2024-08-31 03:49:27 +0300

    wrap in suspense

--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -3,13 +3,19 @@ import ServiceModalProvider from "@/containers/providers/ServiceModalProvider";
 import store from "@/redux/store";
 import theme from "@/styles/muiTheme";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { Suspense } from "react";
 import { Provider } from "react-redux";
+import { GlobalLoader } from "..";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <MuiThemeProvider theme={theme}>
       <Provider store={store}>
-        <ServiceModalProvider>{children}</ServiceModalProvider>
+        <ServiceModalProvider>
+          <Suspense fallback={<GlobalLoader isOpen={true} />}>
+            {children}
+          </Suspense>
+        </ServiceModalProvider>
       </Provider>
     </MuiThemeProvider>
   );
